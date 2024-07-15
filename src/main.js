a5331ca32b9a5e76765ebf06c28b665017b9a28e
Nastya-20 <62868214+Nastya-20@users.noreply.github.com>
2024-07-15 22:19:32 +0300

    add

--- src/main.js
+++ src/main.js
@@ -2,38 +2,45 @@ document.getElementById('order-button').addEventListener('click', function() {
 document.getElementById('order-form').scrollIntoView({ behavior: 'smooth' });
 });
 
+
 // Открытие и закрытие мобильного меню
 const mobMenu = document.querySelector('.mob-menu');
 const modalOverlay = document.querySelector('.modal-overlay');
 const closeMobMenuBtn = document.querySelector('.close-menu-btn');
-const headerMobButton = document.querySelector('.header-mob-button');
+
 
 // Открытие и закрытие таб-меню
 const tabMenu = document.querySelector('.tab-menu');
 const closeTabBtn = document.querySelector('.close-tab-btn');
 const headerButton = document.querySelector('.header-button');
+const openMobMenuBtn = document.querySelector('.open-menu-btn');
 
 // Функции для открытия и закрытия меню
 function openMenu(menu) {
-    menu.classList.add('open');
+    console.log('Opening menu:', menu);
+    menu.classList.add('is-open');
+    modalOverlay.classList.add('is-open');
 }
-
 function closeMenu(menu) {
-    menu.classList.remove('open');
+    console.log('Closing menu:', menu);
+    menu.classList.remove('is-open');
+    modalOverlay.classList.remove('is-open');
 }
 
-// Обработчики событий для открытия и закрытия мобильного меню
-headerMobButton.addEventListener('click', () => openMenu(mobMenu));
+ // Обработчики событий для открытия и закрытия мобильного меню
+openMobMenuBtn.addEventListener('click', () => openMenu(mobMenu));
 closeMobMenuBtn.addEventListener('click', () => closeMenu(mobMenu));
 
-// Обработчики событий для открытия и закрытия таб-меню
-headerButton.addEventListener('click', () => openMenu(modalOverlay));
-closeTabBtn.addEventListener('click', () => closeMenu(modalOverlay));
+// Обработчик события для кнопки "Shop now"
+document.getElementById('order-btn').addEventListener('click', function() {
+      closeMenu(mobMenu);
+document.getElementById('order-form').scrollIntoView({ behavior: 'smooth' });
+});
 
-// Закрытие меню при клике на оверлей
+        // Закрытие меню при клике на оверлей
 modalOverlay.addEventListener('click', (event) => {
     if (event.target === modalOverlay) {
-        closeMenu(modalOverlay);
-    }
+      closeMenu(mobMenu);
+   }
 });
 
